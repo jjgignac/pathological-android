cbbdbcc62263880ef5a0175008308b66f5272370
John-Paul Gignac <jp@gignac.org>
2012-07-15 17:23:02 -0400

    Work on in-game layout.

--- src/org/gignac/jp/pathological/MainActivity.java
+++ src/org/gignac/jp/pathological/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.in_game);
     }
 }
