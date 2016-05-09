1c62414d65f4b0f24c28e31684216eefbe62c609
John-Paul Gignac <jp@gignac.org>
2016-05-09 16:26:52 -0400

    Add license file and menu item

--- app/src/main/java/org/gignac/jp/pathological/MainActivity.java
+++ app/src/main/java/org/gignac/jp/pathological/MainActivity.java
@@ -1,9 +1,14 @@
 package org.gignac.jp.pathological;
 
 import android.app.*;
+import android.net.Uri;
 import android.os.*;
 import android.content.pm.*;
 import android.content.*;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.PopupMenu;
 
 @SuppressWarnings("unused")
 public class MainActivity extends Activity
@@ -42,5 +47,37 @@ public class MainActivity extends Activity
     public void onResume() {
         super.onResume();
         v.onResume();
+    }
+
+    public void showIntroMenu(View v) {
+        final PopupMenu menu = new PopupMenu(this, v);
+        menu.getMenuInflater().inflate(R.menu.intromenu, menu.getMenu());
+        menu.show();
+    }
+
+    public void showLicenceDialog(MenuItem item) {
+        WebView wv = new WebView(this);
+        wv.loadUrl("file:///android_asset/gpl-3.0-standalone.html");
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.getSettings().setUseWideViewPort(true);
+
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.app_name) + " " + getString(R.string.license))
+                .setView(wv)
+                .setPositiveButton(R.string.open_in_browser,new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://www.gnu.org/licenses/gpl.html")));
+                    }
+                })
+                .setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
