97eedf74650b08b90f7440bbb452616bf696bf23
John-Paul Gignac <jp@gignac.org>
2016-05-21 15:14:09 -0400

    Designate several devices as test devices

--- app/src/main/java/org/gignac/jp/pathological/Util.java
+++ app/src/main/java/org/gignac/jp/pathological/Util.java
@@ -33,9 +33,19 @@ class Util {
             // Register as a test device
             AdRequest adReq = adRequestBuilder.addTestDevice(adMobID(context)).build();
             if( !adReq.isTestDevice(context)) return null;
+            return adReq;
         }
 
-        return adRequestBuilder.build();
+        return adRequestBuilder
+                .addTestDevice("47DECF56F1E36BAEA5FEC344FFACB874")
+                .addTestDevice("DE1CE9B2B307F771D0C635CFF3A49F8A")
+                .addTestDevice("9A20DA6E0FBB8699885FAD1E2BEBF607")
+                .addTestDevice("D1C4778238402CC248456A79E281A922")
+                .addTestDevice("21AFB918F9EB700A4AB38096A4FBE118")
+                .addTestDevice("CF0C73A5D37E84C2B3CE5C265E6995D9")
+                .addTestDevice("9A4DB691E32CF695F7127AC6A0FF6C82")
+                .addTestDevice("625B8033AB5B3D3DE8A5B60835E4D783")
+                .build();
     }
 
     public static String adMobID(Context context) {
